03db6636cd7c3d77677c0485d60bf44e5de7d484
Harvey Braun <63766520+harveybraun@users.noreply.github.com>
2020-04-19 21:09:28 -0300

    Add files via upload

--- Spring - Maven - Lombok/src/main/java/braun/harvey/exemplo/ExemploApplication.java
+++ Spring - Maven - Lombok/src/main/java/braun/harvey/exemplo/ExemploApplication.java
@@ -3,6 +3,13 @@ package braun.harvey.exemplo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Descreve a Classe ExemploApplication
+ *
+ * @author Harvey Braun
+ * @version 1.0
+ * @since 18/04/2020 - 14:00
+ */
 @SpringBootApplication
 public class ExemploApplication {
 
